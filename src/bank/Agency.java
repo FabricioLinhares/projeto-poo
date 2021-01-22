9c968c691b3ece685380a929f802734a11917134
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2021-01-22 17:55:33 -0300

    alteração em agency e em Client

--- src/bank/Agency.java
+++ src/bank/Agency.java
@@ -1,4 +1,29 @@
 package bank;
 
+import java.util.List;
+
 public class Agency {
+    private List<Integer> account;
+    private short numberOfAccounts;
+
+    public short createAccount() {
+        numberOfAccounts += 1;
+        return numberOfAccounts;
+    }
+
+    public List<Integer> getContas() {
+        return account;
+    }
+
+    public void setContas(List<Integer> account) {
+        this.account = account;
+    }
+
+    public short getNumberOfAccounts() {
+        return numberOfAccounts;
+    }
+
+    public void setNumberOfAccounts(short numberOfAccounts) {
+        this.numberOfAccounts = numberOfAccounts;
+    }
 }
--- src/bank/Client.java
+++ src/bank/Client.java
@@ -1,4 +1,28 @@
 package bank;
 
 public class Client {
+    private String name;
+    private short[] accountCode;
+
+    public Client(String name, short[] accountCode) {
+        this.name = name;
+        this.accountCode = accountCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public short[] getAccountCode() {
+        return accountCode;
+    }
+
+    public void setAccountCode(short[] accountCode) {
+        this.accountCode = accountCode;
+    }
 }
