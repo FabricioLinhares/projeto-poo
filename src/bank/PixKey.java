505c7353464c1df98e0a239eb1085508bc3557fa
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-01-22 16:57:22 -0300

    implementar PixKey

--- src/bank/PixKey.java
+++ src/bank/PixKey.java
@@ -1,4 +1,56 @@
 package bank;
 
-public class PixKey {
+public class PixKey implements PixTypes {
+    private final byte type;
+    private final String value;
+    private final String owner;
+    private final byte bank;
+    private final byte agency;
+    private final byte account;
+
+    public PixKey(byte type, String value, String owner, byte bank, byte agency, byte account) {
+        this.type = type;
+        this.value = value;
+        this.owner = owner;
+        this.bank = bank;
+        this.agency = agency;
+        this.account = account;
+    }
+
+    public String getType() {
+        String typeString;
+
+        switch (type) {
+            case EMAIL_TYPE:
+                typeString = "Email";
+            case PHONE_TYPE:
+                typeString = "Telefone";
+            case RANDOM_CODE_TYPE:
+                typeString = "Chave aleatório";
+            default:
+                typeString = "Inválido";
+        }
+
+        return typeString;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public byte getBank() {
+        return bank;
+    }
+
+    public byte getAgency() {
+        return agency;
+    }
+
+    public byte getAccount() {
+        return account;
+    }
 }
---
+++ src/bank/PixTypes.java
@@ -0,0 +1,7 @@
+package bank;
+
+public interface PixTypes {
+    public final byte EMAIL_TYPE=0;
+    public final byte PHONE_TYPE=1;
+    public final byte RANDOM_CODE_TYPE=3;
+}
