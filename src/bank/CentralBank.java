fe2d820bd10c69bbbcc8636ade8cea327640bcdc
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-01-25 18:44:14 -0300

    funções do central bank

--- src/bank/CentralBank.java
+++ src/bank/CentralBank.java
@@ -1,4 +1,37 @@
 package bank;
 
+import java.util.*;
+
 public class CentralBank {
+    private static List<PixKey> pixKeys;
+    private static List<Bank> banks;
+
+    public CentralBank() {
+        pixKeys = new ArrayList<PixKey>();
+        banks = new ArrayList<Bank>();
+    }
+
+    public String registerPix(byte type, String owner, byte bankCode, byte agencyCode, byte accountCode) {
+
+    }
+
+    public boolean removePix(String key) {
+
+    }
+
+    public PixKey getPix(String key) {
+
+    }
+
+    public byte registerBank() {
+
+    }
+
+    public boolean removeBank(byte bankCode) {
+
+    }
+
+    public Bank getBank(byte bankCode) {
+
+    }
 }
--- src/bank/PixTypes.java
+++ src/bank/PixTypes.java
@@ -3,5 +3,5 @@ package bank;
 public interface PixTypes {
     public final byte EMAIL_TYPE=0;
     public final byte PHONE_TYPE=1;
-    public final byte RANDOM_CODE_TYPE=3;
+    public final byte RANDOM_CODE_TYPE=2;
 }
