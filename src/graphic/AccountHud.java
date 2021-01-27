e8c9b9c27d5bf8acbb7fb67aa1a1429f9bcabd81
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-01-27 18:46:55 -0300

    hud da conta e do cliente

---
+++ src/graphic/AccountHud.java
@@ -0,0 +1,11 @@
+package graphic;
+
+import javax.swing.*;
+
+public class AccountHud {
+    private JButton depositarButton;
+    private JButton transferirButton;
+    private JButton sacarButton;
+    private JTextPane internetBankingTextPane;
+    private JButton cadastrarPIXButton;
+}
---
+++ src/graphic/ClientHud.java
@@ -0,0 +1,9 @@
+package graphic;
+
+import javax.swing.*;
+
+public class ClientHud {
+    private JComboBox comboBox1;
+    private JPanel panel1;
+    private JTextPane escolhaSuaContaTextPane;
+}
